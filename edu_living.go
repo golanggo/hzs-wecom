cae6a3b34d2025bb101b1ce45643cbfd11eb5aaa
黄博文 <1438494380@qq.com>
2024-12-28 11:49:41 +0800

    we_com项目二开新增创建直播接口

---
+++ edu_living.go
@@ -0,0 +1,199 @@
+package wework
+
+import (
+	"github.com/go-laoji/wecom-go-sdk/v2/internal"
+)
+
+type GetUserAllLivingIdRequest struct {
+	UserId string `json:"userid" validate:"required"`
+	Cursor string `json:"cursor,omitempty"`
+	Limit  int    `json:"limit"`
+}
+
+type GetUserAllLivingIdResponse struct {
+	internal.BizResponse
+	NextCursor   string   `json:"next_cursor"`
+	LivingIdList []string `json:"livingid_list"`
+}
+
+// GetUserAllLivingId 获取老师直播ID列表
+// https://open.work.weixin.qq.com/api/doc/90001/90143/93856
+func (ww *weWork) GetUserAllLivingId(corpId uint, request GetUserAllLivingIdRequest) (resp GetUserAllLivingIdResponse) {
+	if ok := validate.Struct(request); ok != nil {
+		resp.ErrCode = 500
+		resp.ErrorMsg = ok.Error()
+		return
+	}
+	_, err := ww.getRequest(corpId).SetBody(request).SetResult(&resp).
+		Post("/cgi-bin/living/get_user_all_livingid")
+	if err != nil {
+		resp.ErrCode = 500
+		resp.ErrorMsg = err.Error()
+	}
+	return
+}
+
+type GetLivingInfoResponse struct {
+	internal.BizResponse
+	LivingInfo struct {
+		Theme          string `json:"theme"`
+		LivingStart    int    `json:"living_start"`
+		LivingDuration int    `json:"living_duration"`
+		AnchorUserId   string `json:"anchor_userid"`
+		LivingRange    struct {
+			PartyIds   []int    `json:"partyids"`
+			GroupNames []string `json:"group_names"`
+		} `json:"living_range"`
+		ViewerNum     int    `json:"viewer_num"`
+		CommentNum    int    `json:"comment_num"`
+		OpenReplay    int    `json:"open_replay"`
+		PushStreamURL string `json:"push_stream_url"`
+	} `json:"living_info"`
+}
+
+// GetLivingInfo 获取直播详情
+// https://open.work.weixin.qq.com/api/doc/90001/90143/93857
+func (ww *weWork) GetLivingInfo(corpId uint, liveId string) (resp GetLivingInfoResponse) {
+	_, err := ww.getRequest(corpId).SetResult(&resp).
+		SetQueryParam("livingid", liveId).Get("/cgi-bin/school/living/get_living_info")
+	if err != nil {
+		resp.ErrCode = 500
+		resp.ErrorMsg = err.Error()
+	}
+	return
+}
+
+type GetWatchStatRequest struct {
+	LivingId string `json:"livingid" validate:"required"`
+	NextKey  string `json:"next_key"`
+}
+
+type GetWatchStatResponse struct {
+	internal.BizResponse
+	Ending     int    `json:"ending"`
+	NextKey    string `json:"next_key"`
+	StatInfoes struct {
+		Students []struct {
+			StudentUserid string `json:"student_userid"`
+			ParentUserid  string `json:"parent_userid"`
+			Partyids      []int  `json:"partyids"`
+			WatchTime     int    `json:"watch_time"`
+			EnterTime     int    `json:"enter_time"`
+			LeaveTime     int    `json:"leave_time"`
+			IsComment     int    `json:"is_comment"`
+		} `json:"students"`
+		Visitors []struct {
+			Nickname  string `json:"nickname"`
+			WatchTime int    `json:"watch_time"`
+			EnterTime int    `json:"enter_time"`
+			LeaveTime int    `json:"leave_time"`
+			IsComment int    `json:"is_comment"`
+		} `json:"visitors"`
+	} `json:"stat_infoes"`
+}
+
+// GetWatchStat 获取观看直播统计
+// https://open.work.weixin.qq.com/api/doc/90001/90143/93858
+func (ww *weWork) GetWatchStat(corpId uint, request GetWatchStatRequest) (resp GetWatchStatResponse) {
+	if ok := validate.Struct(request); ok != nil {
+		resp.ErrCode = 500
+		resp.ErrorMsg = ok.Error()
+		return
+	}
+	_, err := ww.getRequest(corpId).SetBody(request).SetResult(&resp).
+		Post("/cgi-bin/school/living/get_watch_stat")
+	if err != nil {
+		resp.ErrCode = 500
+		resp.ErrorMsg = err.Error()
+	}
+	return
+}
+
+type GetUnWatchStatResponse struct {
+	internal.BizResponse
+	Ending   int    `json:"ending"`
+	NextKey  string `json:"next_key"`
+	StatInfo struct {
+		Students []struct {
+			StudentUserid string `json:"student_userid"`
+			ParentUserid  string `json:"parent_userid"`
+			Partyids      []int  `json:"partyids"`
+		} `json:"students"`
+	} `json:"stat_info"`
+}
+
+// GetUnWatchStat 获取未观看直播统计
+// https://open.work.weixin.qq.com/api/doc/90001/90143/93859
+func (ww *weWork) GetUnWatchStat(corpId uint, request GetWatchStatRequest) (resp GetUnWatchStatResponse) {
+	if ok := validate.Struct(request); ok != nil {
+		resp.ErrCode = 500
+		resp.ErrorMsg = ok.Error()
+		return
+	}
+	_, err := ww.getRequest(corpId).SetBody(request).SetResult(&resp).
+		Post("/cgi-bin/school/living/get_unwatch_stat")
+	if err != nil {
+		resp.ErrCode = 500
+		resp.ErrorMsg = err.Error()
+	}
+	return
+}
+
+// DeleteReplayData 删除直播回放
+// https://open.work.weixin.qq.com/api/doc/90001/90143/93860
+func (ww *weWork) DeleteReplayData(corpId uint, livingId string) (resp internal.BizResponse) {
+	h := H{}
+	h["livingid"] = livingId
+	_, err := ww.getRequest(corpId).SetBody(h).SetResult(&resp).
+		Post("/cgi-bin/living/delete_replay_data")
+	if err != nil {
+		resp.ErrCode = 500
+		resp.ErrorMsg = err.Error()
+	}
+	return
+}
+
+type LivingCreateRequest struct {
+	AnchorUserID   string `json:"anchor_userid" validate:"required"`
+	Theme                string `form:"theme" json:"theme"`
+	LivingStart          uint32 `form:"living_start" json:"living_start"`
+	LivingDuration       string `form:"living_duration" json:"living_duration"`
+	Type                 int    `form:"type" json:"type"`
+	Description          string `form:"description" json:"description"`
+	AgentID              int    `form:"agent_id" json:"agent_id"`
+	RemindTime           uint32 `form:"remind_time" json:"remind_time"`
+	ActivityCoverMediaID string `form:"activity_cover_media_id" json:"activity_cover_media_id"`
+	ActivityDetail       ActivityDetailInfo `form:"activity_detail" json:"activity_detail"`
+}
+type ActivityDetailInfo struct {
+	Description string   `json:"title"`
+	ImageList   []string `json:"image_list"`
+}
+
+type LivingCreateResponse struct {
+	internal.BizResponse
+	LivingID string `json:"livingid"`
+}
+
+// CreateLiving 创建预约直播
+// https://developer.work.weixin.qq.com/document/path/93717
+func (ww *weWork) LivingCreate(corpId uint, request LivingCreateRequest) (resp LivingCreateResponse) {
+	h := H{}
+	h["anchor_userid"] = request.AnchorUserID
+	h["theme"] = request.Theme
+	h["living_start"] = request.LivingStart
+	h["living_duration"] = request.LivingDuration
+	h["type"] = request.Type
+	h["description"] = request.Description
+	h["agent_id"] = request.AgentID
+	h["remind_time"] = request.RemindTime
+	h["activity_cover_media_id"] = request.ActivityCoverMediaID
+	h["activity_detail"] = request.ActivityDetail
+	_, err := ww.getRequest(corpId).SetBody(h).SetResult(&resp).
+		Post("/cgi-bin/living/create")
+	if err != nil {
+		resp.ErrCode = 500
+		resp.ErrorMsg = err.Error()
+	}
+	return
+}
---
+++ example/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/go-laoji/wecom-go-sdk/v2"
+	"github.com/go-laoji/wecom-go-sdk/v2/config"
+	"github.com/go-laoji/wecom-go-sdk/v2/pkg/demo"
+	"github.com/go-laoji/wecom-go-sdk/v2/pkg/svr"
+	"github.com/go-laoji/wecom-go-sdk/v2/pkg/svr/logic"
+	"github.com/go-laoji/wecom-go-sdk/v2/pkg/svr/middleware"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+)
+
+func main() {
+	c := config.ParseFile("")
+	wwconfig := wework.WeWorkConfig{
+		CorpId:              c.CorpId,
+		ProviderSecret:      c.ProviderSecret,
+		SuiteId:             c.SuiteId,
+		SuiteSecret:         c.SuiteSecret,
+		SuiteToken:          c.SuiteToken,
+		SuiteEncodingAesKey: c.SuiteEncodingAesKey,
+		Dsn:                 c.Dsn,
+	}
+
+	router := gin.Default()
+	ww := wework.NewWeWork(wwconfig)
+	logic.Migrate(wwconfig.Dsn)
+	router.Use(middleware.InjectSdk(ww))
+
+	svr.InjectRouter(router)
+	demo.InjectRouter(router)
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"data": ww.UserGet(1, "jifengwei")})
+	})
+	srv01 := &http.Server{
+		Addr:           fmt.Sprintf("127.0.0.1:%v", c.Port),
+		Handler:        router,
+		ReadTimeout:    15 * time.Second,
+		WriteTimeout:   15 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	go func() {
+		if err := srv01.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatal(err)
+		}
+	}()
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv01.Shutdown(ctx); err != nil {
+		log.Fatal("server shutdown:", err)
+	}
+}
