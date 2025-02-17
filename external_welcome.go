0abac673586148b181277685fe93c729ba763c31
黄博文 <1438494380@qq.com>
2025-02-17 19:25:51 +0800

    新增结构体

--- external_welcome.go
+++ external_welcome.go
@@ -4,9 +4,15 @@ import (
 	"github.com/golanggo/hzs-wecom/internal"
 )
 
+type WelcomeMessage struct {
+	WelcomeCode string                `json:"welcome_code"`
+	Text        ExternalText          `json:"text,omitempty"`
+	Attachments []ExternalAttachments `json:"attachments,omitempty"`
+}
+
 // SendWelcomeMsg 发送新客户欢迎语
 // https://open.work.weixin.qq.com/api/doc/90001/90143/92599
-func (ww *weWork) SendWelcomeMsg(corpId uint, msg ExternalMsg) (resp internal.BizResponse) {
+func (ww *weWork) SendWelcomeMsg(corpId uint, msg WelcomeMessage) (resp internal.BizResponse) {
 	if ok := validate.Struct(msg); ok != nil {
 		resp.ErrCode = 500
 		resp.ErrorMsg = ok.Error()
